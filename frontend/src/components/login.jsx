ea46803c0b554b5968e94caefb104058d68ced90
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-02-09 16:58:06 +0100

    Initial frontend commit

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -4,12 +4,22 @@ import { useNavigate } from "react-router-dom";
 import { FcGoogle } from 'react-icons/fc'
 import shareVideo from '../assets/share.mp4'
 import logo from '../assets/logowhite.png'
+import { client } from '../client';
 
 const Login = () => {
 
     const responseGoogle = (response) => {
         localStorage.setItem('user', JSON.stringify(response.profileObj))
         const {name, googleId, imageUrl} = response.profileObj
+        const doc = {
+            _id: googleId,
+            _type: 'user',
+            userName: name,
+            image: imageUrl,
+        };
+        client.createIfNotExists(doc).then(() => {
+            navigate('/', { replace: true });
+        });
 
     }
 
